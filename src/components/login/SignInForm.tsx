f13ec9520289ed2f77fef27b6b158855c3dcbc44
Abdul Hanan <hanani.nas@gmail.com>
2023-10-11 15:37:36 +0200

    fix for login

--- src/components/login/SignInForm.tsx
+++ src/components/login/SignInForm.tsx
@@ -56,6 +56,7 @@ export const SignInForm: React.FC = () => {
       options: {
         queryParams: {
           access_type: "offline",
+          redirectTo: "https://webpage-iota-ecru.vercel.app/account/login",
         },
       },
     });
@@ -68,7 +69,7 @@ export const SignInForm: React.FC = () => {
       options: {
         queryParams: {
           access_type: "offline",
-          prompt: "consent",
+          redirectTo: "https://webpage-iota-ecru.vercel.app/account/login",
         },
       },
     });
